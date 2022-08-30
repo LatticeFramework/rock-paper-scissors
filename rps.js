81fe987155076d7ae1aecf256ddc37eb0f45fe80
LatticeFramework <10vv3cl@gmail.com>
2022-08-30 12:56:10 -0400

    Add playRound and getComputerChocie functions after adding to stage

--- rps.js
+++ rps.js
@@ -1,7 +1,17 @@
 let randomNumber = Math.random();
-console.log(randomNumber);
+
+
+const playerSelection = "rOck";
+console.log(playerSelection);
+
+const computerSelection = getComputerChoice();
+
+const PSCase = playerSelection.toUpperCase();
+const CSCase = computerSelection.toUpperCase();
+
 
 function getComputerChoice() {
+
     
 
     if (randomNumber < 0.33) {
@@ -10,7 +20,48 @@ function getComputerChoice() {
         return 'paper';
     } else if (0.66 <= randomNumber) {
         return 'scissors';
-    }   
+    }
 }
 
-console.log(getComputerChoice());
+    console.log(getComputerChoice())
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+
+
+    if (PSCase == CSCase) {
+        return "Draw";
+    } else if (PSCase == "ROCK" && CSCase == "PAPER") {
+        return "You lose! Paper beats Rock!";
+    } else if (PSCase == "ROCK" && CSCase == "SCISSORS") {
+        return "You win! Rock beats Scissors";
+    } else if (PSCase == "PAPER" && CSCase == "ROCK") {
+        return "You win! Paper beats Rock";
+    } else if (PSCase == "PAPER" && CSCase == "SCISSORS") {
+        return "You lose! Scissors beats Paper";
+    } else if (PSCase == "SCISSORS" && CSCase == "ROCK") {
+        return "You lose! Rock beats Scissors"; 
+    } else if (PSCase == "SCISSORS" && CSCase == "PAPER") {
+        return "You Win! Scissors beats Paper";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(" ");
+console.log("loop");
+console.log(" ");
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+        playRound();
+        console.log(playRound())
+
+    }
+}
+
+game();
+
